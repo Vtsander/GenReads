4eecb862ff78a73f199c7d4da01761a90ec7ea79
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-01-27 09:46:03 -0800

    more JS to generate

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,26 @@
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(res) {
+function renderLicenseBadge(lic) {
   
-  if (res.license == 'MIT'){
+  if (lic.license == 'MIT'){
     console.log('MIT Chosen')
     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   } 
 
-  if(!res.license){
+  if(!lic.license){
     return ''
   }
 }
-// Function that returns the license link
+// Function that returns the license info
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseInfo(lic) {
+  if(lic.license) {
+    return "##license ~This project is under ${lic.license}~"
+  }
+  else {
+    return ""
+  }
+}
 
 // Function that returns the license section of README
 // If there is no license, return an empty string
