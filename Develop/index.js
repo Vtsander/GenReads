b627f4884a52771d8b8fdcb5298370160ee7bfe8
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-01-28 20:06:02 -0800

    added the questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,11 +5,101 @@ const util = require('util')
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // Array of questions for user input
-const questions = [];
+const questions = [    
+    {
+        type: 'input',
+        name: 'title',
+        message: 'enter project title'
+    },{
+        type: 'confirm',
+        name: 'table',
+        message: 'include table of contents?'
+    },{
+        type: 'checkbox',
+        name: 'sections',
+        message: 'what sections would you like to include in the table of contents?',
+        choices: ['Description', 'Installation', 'Usage', 'License', 'Contributing', 'Tests', 'Questions'],
+        when (answers) {
+            return answers.table
+        }
+    },{
+        type: 'confirm',
+        name: 'helper',
+        message: 'would you like to enable description helper?'
+    },{
+        type: 'input',
+        name: 'function',
+        message: 'what does your application do?',
+        when (answers){
+            return answers.helper
+        }
+    },{
+        type: 'checkbox',
+        name: 'tech',
+        message: 'what tech did you use?',
+        choices: ['html', 'css', 'javascript', 'node.js'],
+        when (answers) {
+            return answers.helper
+        }
+    },{
+        type: 'input',
+        name: 'challenges',
+        message: 'what challenges did you face?',
+        when (answers) {
+            return answers.helper
+        }
+    },{
+        type: 'input',
+        name: 'custom',
+        message: 'enter custom description',
+        when (answers) {
+            return !answers.helper
+        }
+    },{
+        type: 'list',
+        name: 'license',
+        message: 'which license would you like to use?',
+        choices: ['MIT', 'Apache 2.0', 'Mozilla Public 2.0', 'GNU GPL'],
+        when (answers) {
+            let validate = 'License'
+            return answers.sections && answers.sections.includes(validate)
+    }
+    },{
+        type: 'input',
+        name: 'contributing',
+        message: 'enter contributor name(s)',
+        when (answers) {
+            let validate = 'Contributing'
+            return answers.sections && answers.sections.includes(validate)
+    }
+    },{
+        type: 'input',
+        name: 'questionsGit',
+        message: 'enter github username',
+        when (answers) {
+            let validate = 'Questions'
+            return answers.sections && answers.sections.includes(validate)
+        }
+    },{
+        type: 'input',
+        name: 'questionsEmail',
+        message: 'enter your email address',
+        when  (answers) {
+            let validate = 'Questions'
+            return answers.sections && answers.sections.includes(validate)
+        }
+
+    }
+];
 
 // Function to write README file
 function writeToFile(fileName, data) {
-
+    fs.writeFile("${fileName}", generateMarkdown(data),
+    function(err){
+        if (err)
+        return console.log(err)
+    })
+    console.log('Your README has been generated');
 }
 
 // Function to initialize app
